ab90904a0ff2844c7e27deaea213aa27ab9045e3
Ashwani Chauhan <ashwanichauhan092@gmail.com>
2023-11-20 22:30:42 +0530

    Reverted Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// App.js
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import BlogList from "./components/BlogList";
@@ -12,33 +11,25 @@ import ProjectHome from "./components/ProjectHome";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
-import "./App.css"; // Import the CSS file
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
+    return (
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
 
-  return (
-    <Router>
-      <div className={`app ${darkMode ? "dark-mode" : ""}`}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/Home" element={<ProjectHome />} />
-          <Route path="/contact" element={<ContactForm />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-        <button onClick={toggleDarkMode}>
-          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
-        </button>
-      </div>
-    </Router>
-  );
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<LogIn />} />
+                <Route path="/Home" element={<ProjectHome />} />
+                <Route path="/contact" element={<ContactForm />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+            <Footer />
+        </Router>
+
+
+    );
 };
 
 export default App;
